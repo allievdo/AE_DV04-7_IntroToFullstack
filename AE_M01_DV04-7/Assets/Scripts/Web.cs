0588beb507a1309e0288466e857cb1ed24633335
Allison Evdokimo <allison.evdokimo@cox.net>
2025-02-12 09:48:06 -0700

    AddedDate

---
+++ AE_M01_DV04-7/Assets/Scripts/Web.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Web : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		StartCoroutine(GetText());
+	}
+
+	IEnumerator GetText()
+	{
+		using (UnityWebRequest www = UnityWebRequest.Get("http://localhost/UnityBackendTutorial/GetDate.php"))
+		{
+			yield return www.Send();
+
+			if (www.isNetworkError || www.isHttpError)
+			{
+				Debug.Log(www.error);
+			}
+			else
+			{
+				//Show results as text
+				Debug.Log(www.downloadHandler.text);
+
+				// Or retrieve results as binary data
+				byte[] results = www.downloadHandler.data;
+			}
+		}
+	}
+	
+}
